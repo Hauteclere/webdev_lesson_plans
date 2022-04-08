cb4a20f763194410afba8cf911ca3d6eccbd2162
Oliver-CoderAcademy <oliver.lavers@coderacademy.edu.au>
2022-04-08 13:44:57 +1000

    created scaffold for alice and bob challenge

---
+++ day_4/alice_and_bob/index.js
@@ -0,0 +1,7 @@
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/entries
+
+function cullList(arr, num) {
+
+}
+
+module.exports = cullList;
---
+++ day_4/alice_and_bob/index.test.js
@@ -0,0 +1,12 @@
+/* 
+-> Accepts a list and a number
+-> throws error if arguments are in wrong order
+-> throws error if arguments are not array and number
+-> throws error if array contains non-numbers
+-> output should be a list
+-> output should have no more than n repeats
+-> output should include motifs from input (unless n=0)
+ */
+
+const cullList = require("./index");
+
