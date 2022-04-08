493c0fbff37b91bc1eb54791197d42fe18df8380
Oliver-CoderAcademy <oliver.lavers@coderacademy.edu.au>
2022-04-08 15:04:35 +1000

    solved alice and bob challenge

--- day_4/alice_and_bob/index.js
+++ day_4/alice_and_bob/index.js
@@ -1,7 +1,32 @@
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/sign
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Number/isInteger
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/entries
 
 function cullList(arr, num) {
+    if ((Math.sign(num)===-1) || !Number.isInteger(num)) {
+        throw "Bad number argument!";
+    }
 
+    try {
+        const resultObject = {};
+        const resultArray = [];
+        for (let element of arr) {
+            if (typeof element !== "number") {
+                return "Bad array!";
+            }
+
+            resultObject[element] = resultObject[element]+1 || 1;
+            
+            if (resultObject[element]<=num) {
+                resultArray.push(element);
+            }
+        }
+        return resultArray;
+    }
+    catch (error) {
+        return "Bad argument!";
+    }
 }
+
 
 module.exports = cullList;
--- day_4/alice_and_bob/index.test.js
+++ day_4/alice_and_bob/index.test.js
@@ -1,12 +1,35 @@
 /* 
--> Accepts a list and a number
--> throws error if arguments are in wrong order
--> throws error if arguments are not array and number
--> throws error if array contains non-numbers
--> output should be a list
--> output should have no more than n repeats
--> output should include motifs from input (unless n=0)
- */
+-> output error msg if first arg is not array
+-> output error msg if array contains non-numbers
+-> throw exception if second arg is not positive integer
+-> returns array
+-> output contains no more than n repeats (where n == second arg)
+-> output contains all input motifs
+
+*/
 
 const cullList = require("./index");
 
+test('returns error msg if first arg != array', () => {
+    expect(cullList(4, 1)).toBe("Bad argument!");
+});
+
+test('outputs error msg if array has non-numbers', () => {
+    expect(cullList([true, null, "hello"], 2)).toBe("Bad array!");
+});
+
+test('throws exception is second arg is not positive int', () => {
+    expect(() => { cullList([1, 1, 3], "hi") }).toThrow();
+});
+
+test('returns an array', () => {
+    expect(Array.isArray(cullList([1, 1, 1, 1, 3], 2))).toEqual(true);
+});
+
+test('returns array with no more than n repeats', () => {
+    expect(cullList([1, 1, 1, 1, 3], 2)).toStrictEqual([1, 1, 3]);
+});
+
+test('doesnt cull unnecessarily', () => {
+    expect(cullList([2, 2, 2, 2, 2, 4, 4, 4, 5], 3)).toEqual(expect.arrayContaining([2, 4, 5]));
+});
