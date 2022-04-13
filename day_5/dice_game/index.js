5f9d5a4981c74d17908d52d6714240f21efa5c50
Oliver-CoderAcademy <oliver.lavers@coderacademy.edu.au>
2022-04-14 09:33:07 +1000

    removed the solution whoops

--- day_5/dice_game/index.js
+++
@@ -1,6 +0,0 @@
-import { changeImgSrc} from "./src/utils.js"
-
-const button = document.getElementById("button");
-const image = document.querySelector("img");
-
-button.addEventListener("click", () => { changeImgSrc(image) });
--- day_5/dice_game/src/utils.js
+++
@@ -1,8 +0,0 @@
-export function randomNumber() {
-    return Math.ceil(Math.random()*6);
-}
-
-export function changeImgSrc(imageElement) {
-    imageElement.src = `./images/${randomNumber()}.png`;
-}
-
